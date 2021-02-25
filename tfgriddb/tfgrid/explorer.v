d453aaddf95911b21db107925ccf9de2f945f1c6
dylanVerstraete <dylan_verstraete1@hotmail.com>
2021-02-25 14:39:59 +0100

    fix: fix entity get by id

--- tfgriddb/tfgrid/explorer.v
+++ tfgriddb/tfgrid/explorer.v
@@ -2,9 +2,6 @@ module tfgrid
 
 import net.http
 import json
-import strconv
-
-// logic needs to be here to fetch basic info from TFGrid DB
 
 pub struct Explorer {
 	ipaddr string
@@ -20,26 +17,10 @@ pub fn explorer_new() ?Explorer {
 	return explorer
 }
 
-pub struct VarValue {
-	typ VarType
-	value string
-}
-
-pub enum VarType {
-	string = 0
-	number = 1
-	bool = 2	
-}
-
 struct GraphqlQuery {
 mut:
 	query string
 	operation string
-	variables string
-}
-
-struct Variable {
-	id string
 }
 
 struct ReqData {
@@ -69,22 +50,15 @@ pub fn (mut explorer Explorer) entity_list() ? []TFGridEntity {
 }
 
 pub fn (mut explorer Explorer) entity_by_id (id string) ? TFGridEntity {
-	var := parse_var("id", VarValue{typ: VarType.string, value: id})
-	println(var)
-
 	mut query := GraphqlQuery{
-		query: '{ entities(where: {entityId_eq: \$id }) { name } }',
+		query: '{ entities(where: {entityId_eq: $id }) { name, entityId, name, gridVersion, countryId, cityId } }',
 		operation: 'getOne',
 	}
 
-	query.variables = var
-
 	req := make_post_request_query(explorer.ipaddr, query)?
 
-	println(req)
-
 	res := req.do() ?
-	println(res)
+
 	data := json.decode(ReqData, res.text) or {
 		eprintln('failed to decode json')
 		return TFGridEntity{}
@@ -105,46 +79,4 @@ pub fn make_post_request_query(url string, query GraphqlQuery) ?http.Request {
 	req.add_header('Content-Type', 'application/json')
 
 	return req
-}
-
-fn parse_var(key string, _type VarValue) string {
-	if _type.typ == VarType.string {
-		stru := &StringVar {
-			key: key,
-			value: _type.value
-		}
-
-		return json.encode(stru)
-	} else if _type.typ == VarType.number {
-		stru := &IntVar {
-			key: key,
-			value: strconv.atoi(_type.value)
-		}
-
-		return json.encode(stru)
-	} else if _type.typ == VarType.bool {
-		stru := &BooleanVar {
-			key: key,
-			value: _type.value == "true"
-		}
-
-		return json.encode(stru)
-	}
-
-	return ""
-}
-
-struct IntVar {
-	key string
-	value int
-}
-
-struct BooleanVar {
-	key string
-	value bool
-}
-
-struct StringVar {
-	key string
-	value string
 }
